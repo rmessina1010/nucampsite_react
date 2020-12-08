951ba60b027da1d8adde40564a72c4eed5fd4ddc
Ray Messina <rmessina10@gmail.com>
2020-12-08 03:19:47 -0600

    Combining Reducers

---
+++ src/redux/partners.js
@@ -0,0 +1,7 @@
+import { PARTNERS } from '../shared/partners';
+export const Partners = (state = PARTNERS, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
