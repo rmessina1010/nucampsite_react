7dce343ae217485946fb4db973bf3c9226e91672
Ray Messina <rmessina10@gmail.com>
2020-12-11 17:25:12 -0600

    Fetch from Server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,23 +1,14 @@
-import { CAMPSITES } from '../shared/campsites';
 import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (campsiteId, rating, author, text) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        campsiteId: campsiteId,
-        rating: rating,
-        author: author,
-        text: text
-    }
-});
 
 
 export const fetchCampsites = () => dispatch => {
     dispatch(campsiteLoading());
 
-    setTimeout(() => {
-        dispatch(addCampsite(CAMPSITES));
-    }, 2000);
+    return fetch(baseUrl + 'campsites')
+        .then(response => response.json())
+        .then(campsites => dispatch(addCampsite(campsites)))
 };
 
 
@@ -35,3 +26,51 @@ export const addCampsite = campsites => ({
     payload: campsites
 });
 
+export const addComment = (campsiteId, rating, author, text) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    }
+});
+
+export const fetchComments = () => dispatch => {
+    return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(campsites => dispatch(addComment(campsites)))
+};
+
+export const commentsFailed = errMess => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errMess
+});
+
+
+export const addComments = comments => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromotions = () => dispatch => {
+    dispatch(promotionsLoading());
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promotions => dispatch(addPromotions(promotions)))
+};
+
+export const promotionsLoading = () => ({
+    type: ActionTypes.PROMOTIONS_LOADING
+});
+
+export const promotionsFailed = errMess => ({
+    type: ActionTypes.PROMOTIONS_FAILED,
+    payload: errMess
+});
+
+export const addPromotions = promotions => ({
+    type: ActionTypes.ADD_PROMOTIONS,
+    payload: promotions
+});
